9c7f6ba6c4ed420faef5d07f6de3fcd530c05b62
sukurov2004 <maliksukurov27@gmail.com>
2025-11-21 14:31:29 +0400

    test3

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var questionNumber = prompt("Hansı sualı cavablandırmaq istəyirsən? (1-5)");
+var questionNumber = prompt("Hansı sualı cavablandırmaq istəyirsən? (1-8)");
 
+
+// !Sual-1
 if (questionNumber == 1) {
   var answer = prompt("1) Dünyanın ən böyük okeanı hansıdır?");
   if (answer == "pacific") {
@@ -7,41 +9,69 @@ if (questionNumber == 1) {
   } else {
     alert("Səhv. Düz cavab: Pacific");
   }
-} else if (questionNumber == 2) {
+}
+// !Sual-2
+ else if (questionNumber == 2) {
   var answer = prompt("2) Günəş sistemində neçə planet var?");
   if (Number(answer) == 8) {
     alert("Düz cavab!");
   } else {
     alert("Səhv. Düz cavab: 8");
   }
-} else if (questionNumber == 3) {
+} 
+// !Sual-3
+else if (questionNumber == 3) {
   var answer = prompt("3) İnsan bədənində neçə sümük var?");
   if (Number(answer) == 206) {
     alert("Düz cavab!");
   } else {
     alert("Səhv. Düz cavab: 206");
   }
-} else if (questionNumber == 4) {
+}
+// !Sual-4
+ else if (questionNumber == 4) {
   var answer = prompt("4) Dünyanın ən uzun çayı hansıdır?");
   if (answer.toLowerCase() == "nile") {
     alert("Düz cavab!");
   } else {
     alert("Səhv. Düz cavab: Nile");
   }
-} else if (questionNumber == 5) {
+}
+// !Sual-5
+ else if (questionNumber == 5) {
   var answer = prompt("5) Dünyanın ən yüksək dağı hansıdır?");
   if (answer.toLowerCase() == "everest") {
     alert("Düz cavab!");
   } else {
     alert("Səhv. Düz cavab: Everest");
   }
-} else if (questionNumber == 6) {
+}
+// !Sual-6
+ else if (questionNumber == 6) {
   var answer = prompt("6) Dünyanın ən böyük gölü hansıdı?");
   if (answer.toLocaleLowerCase() == "xəzər") {
     alert("Düz cavab!");
   } else {
     alert("Səhv. Düz cavab: Xəzər");
   }
-} else {
-  alert("Yanlış seçim etdin. 1 ilə 5 arasında rəqəm daxil et.");
+}
+// !Sual-7
+else if (questionNumber == 7) {
+  var answer = prompt("7) Azərbaycan bayrağında neçə rəng var?");
+  if (answer.toLocaleLowerCase() == 3) {
+    alert("Düz cavab!");
+  } else {
+    alert("Səhv. Düz cavab: 3");
+  }
+}
+// !Sual-8
+else if (questionNumber == 8) {
+  var answer = prompt("8) Dünyanın ən böyük okeanı hansıdır?");
+  if (answer.toLocaleLowerCase() == "sakit okean") {
+    alert("Düz cavab!");
+  } else {
+    alert("Səhv. Düz cavab: Sakit okean");
+  }
+}else {
+  alert("Yanlış seçim etdin. 1 ilə 8 arasında rəqəm daxil et.");
 }
